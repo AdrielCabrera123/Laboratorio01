7942216177c70e6b942e80282abe4a6decb6f53c
AdrielCabrera123 <carlos.cabrera.r@tecsup.edu.pe>
2024-08-20 20:58:06 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Triangulo triangulo = new Triangulo
+            {
+                X1 = 3,
+                Y1 = 2,
+                X2 = 3,
+                Y2 = 4,
+                X3 = 5,
+                Y3 = 6
+            };
+
+           
+            double perimetro = triangulo.CalcularPerimetro();
+            double area = triangulo.CalcularArea();
+
+           
+            Console.WriteLine($"Perímetro del triángulo: {perimetro}");
+            Console.WriteLine($"Área del triángulo: {area}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Laboratorio01/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01
+{
+    public class Triangulo
+    {
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+        public double X3 { get; set; }
+        public double Y3 { get; set; }
+
+        private double CalcularDistancia(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        public double CalcularPerimetro()
+        {
+            double a = CalcularDistancia(X1, Y1, X2, Y2);
+            double b = CalcularDistancia(X2, Y2, X3, Y3);
+            double c = CalcularDistancia(X3, Y3, X1, Y1);
+
+            return a + b + c;
+        }
+
+        public double CalcularArea()
+        {
+            double a = CalcularDistancia(X1, Y1, X2, Y2);
+            double b = CalcularDistancia(X2, Y2, X3, Y3);
+            double c = CalcularDistancia(X3, Y3, X1, Y1);
+
+            double s = (a + b + c) / 2;
+
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+    }
+}
